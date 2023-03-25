7d274dc1bf85cf4c470f52d6d4e7cdd338929d85
jinsil <jinsil378900@gmail.com>
2023-03-26 01:33:55 +0900

    Feat: 서버 연결

--- src/pages/Main/List/List.js
+++ src/pages/Main/List/List.js
@@ -2,12 +2,34 @@ import React from "react";
 import { useState, useEffect, useRef } from 'react';
 import {Link} from 'react-router-dom';
 import {useRecoilState} from 'recoil';
+import axios from 'axios';
 import {selectCate} from '../../../Atom';
 import Check from './Check';
 import './List.css'
 
 const List = ()=>{
   const [checkedItems, setcheckedItems] = useState([]);
+  const [items,setItems] = useState(null);   //결과값
+  const [loading,setLoading] = useState(false); // 로딩되는지 여부
+  const [error,setError] = useState(null); //에러
+  const baseUrl = process.env.REACT_APP_BASE_URL;
+
+  useEffect( () =>{
+    fetchItems();
+  },[]);
+
+  const fetchItems = async () => {
+    try {
+        setError(null);
+        setLoading(true); //로딩이 시작됨
+        const response = await axios.get(`${baseUrl}/api/items`);
+        console.log(response)
+        // setItems(response.data.result.postList)
+    } catch (e) {
+        setError(e);
+    }
+    setLoading(false);
+  };
 
   return(
     <div className="list">
